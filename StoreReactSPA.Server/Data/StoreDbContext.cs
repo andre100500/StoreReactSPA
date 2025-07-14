e9f84f39fb14417f3f10819747c514fd4e137c50
Andrs <Andrs@ANDR>
2025-07-14 17:21:20 +0300

    created dbcontext

---
+++ StoreReactSPA.Server/Data/StoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StoreReactSPA.Server.Models;
+
+namespace StoreReactSPA.Server.Data
+{
+    public class StoreDbContext : DbContext 
+    {
+        public DbSet<UserModel> Users { get; set; }
+        public DbSet<SalesProducModel> salesProducModels { get; set; }
+        public DbSet<ProductModel> Products { get; set; }
+        public StoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
