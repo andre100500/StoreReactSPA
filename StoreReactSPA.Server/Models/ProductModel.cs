4275aa042e50e17cfac64f79e1fe5870cf9c93be
Andrs <Andrs@ANDR>
2025-07-14 17:03:31 +0300

    Добавьте файлы проекта.

---
+++ StoreReactSPA.Server/Models/ProductModel.cs
@@ -0,0 +1,12 @@
+﻿namespace StoreReactSPA.Server.Models
+{
+    public class ProductModel
+    {
+        public Guid Id { get; set; }
+        public string NameProduct { get; set; } = string.Empty; 
+        public string DescriptionProduct { get; set; }
+        public string TitleProduct => $"{Category}:{NameProduct}";
+
+        public string Category { get; set; } = string.Empty;
+    }
+}
---
+++ StoreReactSPA.Server/Models/UserModel.cs
@@ -0,0 +1,14 @@
+﻿namespace StoreReactSPA.Server.Models
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string EmailConfirmed { get; set; }= string.Empty;
+        public string PasswordConfirmed { get; set; } = string.Empty;
+
+        public ProductModel Product { get; set; }
+    }
+}
