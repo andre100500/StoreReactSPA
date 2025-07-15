f0fb1769f28051785dda223c27d53e9351c70c8e
Andrs <Andrs@ANDR>
2025-07-15 13:39:51 +0300

    tested controler

---
+++ StoreReactSPA.Server/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreReactSPA.Server.DTOs;
+using StoreReactSPA.Server.Services;
+
+namespace StoreReactSPA.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService) 
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(CreateUserDto createUserDto)
+        {
+            try
+            {
+                var userDto = await _userService.RegisterUserAsync(createUserDto);
+                if (userDto == null) return BadRequest("Invalid request data");
+                return CreatedAtAction(nameof(GetUserById), new {id=userDto.Id }, userDto);
+            }
+            catch (Exception ex) {return BadRequest(ex.Message); }
+        }
+        [HttpGet("{id}")]
+        private object GetUserById(Guid id)
+        {
+            return Ok($"Get user {id}");
+        }
+    }
+}
