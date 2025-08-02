35d33f9f2d9dfdac1385d15fcfeaa8b925614744
Andrs <Andrs@ANDR>
2025-08-03 02:17:38 +0300

    created product controller

---
+++ StoreReactSPA.Server/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StoreReactSPA.Server.Data.Repositories.InterfaceRepositories;
+using StoreReactSPA.Server.DTOs.CreatedDTOs;
+using StoreReactSPA.Server.DTOs.UpdateDTOs;
+using StoreReactSPA.Server.Services.Inteface;
+
+namespace StoreReactSPA.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(_productService.GetAllProductsAsync());
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok(_productService.GetProductByIdAsync(id));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductDto createDto)
+        {
+            var newProduct = await _productService.CreateProductAsync(createDto);
+            return CreatedAtAction(nameof(Create),new { id = newProduct.Id }, newProduct);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, UpdateProductDto updateDto)
+        {
+            return Ok( await _productService.UpdateProductAsync(id, updateDto));
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            await _productService.DeleteProductAsync(id);
+            return NoContent();
+        }
+    }
+}
