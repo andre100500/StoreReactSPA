4ecb6b894b108ba952459a0d2d4ed149abb34bbd
Andrs <Andrs@ANDR>
2025-08-16 18:38:00 +0300

    fix

--- storereactspa.client/src/App.jsx
+++ storereactspa.client/src/App.jsx
@@ -1,62 +1,22 @@
-import { useEffect, useState } from 'react';
+import {useState } from 'react';
+import Login from './Login';
 import './App.css';
 
 function App() {
-    const [forecasts, setForecasts] = useState();
-
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
-
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tableLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
-
+    const [token, setTokenState] = useState(localStorage.getItem('token'));
+    const setToken = (userToken) => {
+        localStorage.setItem('token', userToken);
+        setTokenState(userToken);
+    };
+    if (!token) {
+        return <Login setToken={setToken} />;
+    }
     return (
         <div>
-            <h1 id="tableLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
+            <h1>Admin Panel</h1>
+            {/* Your AdminPanelComponent where you fetch products */}
         </div>
     );
-    
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        if (response.ok) {
-            const data = await response.json();
-            setForecasts(data);
-        }
-    }
-
-    async function fetchProducts() {
-        const token = localStorage.getItem('token');
-
-        const response = await fetch('https://localhost:XXXX/api/products', {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
-    }
 }
 
 export default App;
