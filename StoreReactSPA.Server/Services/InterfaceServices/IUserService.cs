7f67a3b4086a289d7329826897e1a72e41a86a6a
Andrs <Andrs@ANDR>
2025-10-22 16:10:22 +0300

    Updata UserServices add CRUD

--- StoreReactSPA.Server/Services/InterfaceServices/IUserService.cs
+++ StoreReactSPA.Server/Services/InterfaceServices/IUserService.cs
@@ -1,11 +1,15 @@
 ﻿using StoreReactSPA.Server.DTOs;
 using StoreReactSPA.Server.DTOs.CreatedDTOs;
+using StoreReactSPA.Server.DTOs.UpdateDTOs;
 
 namespace StoreReactSPA.Server.Services.Inteface
 {
     public interface IUserService
     {
         Task<UserDto> GetUserByIdAsync(Guid id);
+        Task<IEnumerable<UserDto>> GetAllUsersAsync();
+        Task<UserDto> UpdateUserAsync(Guid id, UpdateUserDto updateUserDto);
         Task<UserDto> RegisterUserAsync(CreateUserDto createUserDto);
+        Task<bool> DeleteUserAsync(Guid id);
     }
 }
